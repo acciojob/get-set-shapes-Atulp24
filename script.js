8bd4ed3004c7349a1307cb87a312798ae643c183
Atulp24 <apk200209@gmail.com>
2025-01-15 19:59:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(a, b) {
+		this._width = a
+		this._height = b
+	}
+	get width() {
+		return this._width
+	}
+	get height() {
+		return this._height
+	}
+	getArea() {
+		return this.width*this.height
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(s) {
+		super(s,s)
+	}
+	getPerimeter() {
+		return this.width*4
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
